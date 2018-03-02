b42c0883d28c3619d292e7d366f796153eea8091
Sunil23 <sunil.sunxiil.choudhary@gmail.com>
2018-03-02 13:57:36 +0530

    Added columns structure with three panel. Date, circle, content

--- common.js
+++ common.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
     	data:[{
     		date:'23 march 1991',
     		event:'My Birthday'
+    	},{
+    		date:'27 january 1993',
+    		event:'Monu Birthday'
+    	},{
+    		date:'22 november 1995',
+    		event:'Chottu Birthday'
     	}]
     });
     
--- timeline.js
+++ timeline.js
@@ -3,6 +3,11 @@
  *
  */
 
+ var TIMELINECONSTANTS = {
+ 	CLASS:'time-line',
+ 	CLASSPREFIX:'time-line-'
+ }
+
 function repeater(arr) {
 	return {
 		each:function(callback) {
@@ -18,12 +23,71 @@ function repeater(arr) {
 $.fn.timeline = function(options) {
 	repeater(this).each(function(element) {
 		var timeLine = new Timeline(options,element);
-	})
+	});
     return this;
+}
+function createPanels(mainPanel) {
+	var datePanel = document.createElement("div");
+	var circlePanel = document.createElement("div");
+	var contentPanel = document.createElement("div");
+	var panels  = {
+		$datePanel:$(datePanel),
+		$circlePanel:$(circlePanel),
+		$contentPanel:$(contentPanel)
+	}
+
+	var prfx = TIMELINECONSTANTS.CLASSPREFIX;
+
+	panels.$datePanel.addClass(prfx+'panel').addClass(prfx+'date-panel');
+	panels.$circlePanel.addClass(prfx+'panel').addClass(prfx+'circle-panel');
+	panels.$contentPanel.addClass(prfx+'panel').addClass(prfx+'content-panel');
+
+
+
+	mainPanel
+	.append(datePanel)
+	.append(circlePanel)
+	.append(contentPanel);
+
+
+	return panels;
 }
 
 
 function Timeline(options,$elem) {
-	console.log($elem);
+	var self = this;
 
+
+	$elem.addClass(TIMELINECONSTANTS.CLASS);
+	self.panels = createPanels($elem);
+
+	options.data.forEach(function(event,index) {
+		self.renderCircle(event,index);
+	});
+
+
+}
+
+Timeline.prototype.renderDate = function renderCircle(event,index) {
+
+}
+
+
+Timeline.prototype.renderCircle = function renderCircle(event,index) {
+
+	var self = this;
+	var circle = document.createElement("div");
+	$circle = $(circle);
+
+	$circle
+	.addClass('time-line-circle');
+
+
+	if(index){
+		$circle
+		.addClass('time-line-circle-margin');
+		
+	}
+
+	self.panels.$circlePanel.append(circle);
 }
